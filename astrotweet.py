17a497817bc1d12f4f3f44c253258060959defdf
Arty52 <artgrichine@gmail.com>
2014-04-17 11:45:13 -0700

    Twitter works!
    Twitter API talks with python program and posts sunrise/set
    Date before adjusted time removed

--- astrotweet.py
+++ astrotweet.py
@@ -2,7 +2,7 @@
 # ArtGrichine@gmail.com
 
 import ephem
-import twitter
+from twitter import *
 import sys
 import sqlite3
 
@@ -31,10 +31,18 @@ def local_time(timezone, gmt_time):
     local_hour = int(time[-8:-6]) + int(time_zones['{}'.format(timezone)])
     if local_hour < 0:
         local_hour = local_hour + 24
-    local_time = time[:-8] + str(local_hour) + time[-6:]
+    # local_time = time[:-8] + str(local_hour) + time[-6:]  #Date is included before time
+    local_time = str(local_hour) + time[-6:]
     return str(local_time)
 
 def main():
+
+    twitter = Twitter(auth=OAuth(
+                    '863439710-u1hYZ3s6NxpAgl8DXyM4sdModp9mKYL1W75IBGQz',     #Access Token
+                    'UEggOxNswZXnQCVnODMUMzKSbkLlpZ9f8jhgIbzkv5yxh',          #Access Token Secret
+                    'PF3WlkPo7Pygaohk1BMZKTt5D',                              #API Key
+                    'aEjd8gtlvPEu31it9MHF0rOPIrWKENiUzQeJ27qQNl2BJFyrHI'))    #API Secret
+    
     city   = sys.argv[1] 
     region = "US/" + sys.argv[2]                                       
     db = sqlite3.connect('us_only.sq3')
@@ -43,8 +51,8 @@ def main():
     c.execute('select latitude, longitude, time_zone from sol_places where name = "{}" and region = "{}";'.format(city, region))
 
     for i in c:
-        latitude  = i[0]
-        longitude = i[1]
+        latitude   = i[0]
+        longitude  = i[1]
         time_zone  = i[2]
         print('Latitude: {}'.format(i[0]))
         print('Longitude: {}'.format(i[1]))
@@ -56,8 +64,11 @@ def main():
     m.compute(ob)
     print(ob.next_rising(m))
     
+    twitter.statuses.update(status = 'The sun will rise at {} and set at {} today.'.format(
+                                                                                        local_time(time_zone, (ob.next_rising(m))), 
+                                                                                        local_time(time_zone, (ob.next_setting(m)))))
     print('Sun will rise: ' + local_time(time_zone, (ob.next_rising(m))))    
-    print('Sun will set: ' + local_time(time_zone, (ob.next_setting(m))))
+    print('Sun will set: '  + local_time(time_zone, (ob.next_setting(m))))
     
 if __name__ == '__main__':
     main()
