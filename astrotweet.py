14bbcc74cc32b0aca3562cb904af5b5f36416653
Arty52 <artgrichine@gmail.com>
2014-04-16 22:29:47 -0700

    Sunrise for observer calculated
    Observer instantiated
    Observers location (coordinate) based on city/state
    local_time() function made, contains dictionary of timezones with
    proper adjustments. Calculates observers timezone from GMT.

--- astrotweet.py
+++ astrotweet.py
@@ -3,8 +3,35 @@ import twitter
 import sys
 import sqlite3
 
+def local_time(timezone, gmt_time):
+    time_zones = {'America/Chicago': -6,
+                  'US/Mountain': -7,
+                  'US/Arizona': -7,
+                  'America/New_York': -5,
+                  'US/Eastern': -5,
+                  'US/Central': -6,
+                  'America/Indianapolis': -5,
+                  'Pacific/Samoa': -11,
+                  'US/Pacific': -8,
+                  'America/Denver': -7,
+                  'America/Puerto_Rico': -4,
+                  'America/Los_Angeles': -8,
+                  'Pacific/Guam': 10,
+                  'America/Adak': -10,
+                  'America/Virgin': -4,
+                  'Pacific/Saipan': 10,
+                  'US/Alaska': -9,
+                  'US/Hawaii': -10, 
+                 }
+                 
+    time = str(gmt_time)
+    local_hour = int(time[-8:-6]) + int(time_zones['{}'.format(timezone)])
+    local_time = time[:-8] + str(local_hour) + time[-6:]
+    return str(local_time)
+
 def main():
-    city =  sys.argv[1]                     
+    city   = sys.argv[1] 
+    region = "US/" + sys.argv[2]                    
  #    state = sys.argv[2]                      
     db = sqlite3.connect('us_only.sq3')
     c = db.cursor()
@@ -16,11 +43,25 @@ def main():
 #     print(x.size)
 #     print(sun.name)
 # select latitude from sol_places where name = "Los Angeles";
-    c.execute('select latitude from sol_places where name = "{}";'.format(city))
+    # c.execute('select latitude from sol_places where name = "{}";'.format(city))
+    c.execute('select latitude, longitude, time_zone from sol_places where name = "{}" and region = "{}";'.format(city, region))
     # nyc = ephem.city('New York')
     # print(nyc.lat)
     for i in c:
-        print('latitude: {}'.format(i))
+        latitude  = i[0]
+        longitude = i[1]
+        time_zone  = i[2]
+        print('Latitude: {}'.format(i[0]))
+        print('Longitude: {}'.format(i[1]))
     
+    ob = ephem.Observer()
+    ob.lat = str(latitude)
+    ob.lon = str(longitude)
+    m = ephem.Sun()
+    m.compute(ob)
+    print(ob.next_rising(m))
+    
+    ob_time = local_time(time_zone, (ob.next_rising(m)))
+    print('Observers time is: ' + ob_time)
 if __name__ == '__main__':
     main()
