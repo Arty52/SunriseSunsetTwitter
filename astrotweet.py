81a52c3cefae6ab419dcbe989edea18fb6d6982f
Arty52 <artgrichine@gmail.com>
2014-04-21 17:46:04 -0700

    phases of the moon
    moon object created
    phases of the moon function started (not finished)

--- astrotweet.py
+++ astrotweet.py
@@ -5,7 +5,12 @@ import ephem
 from twitter import *
 import sys
 import sqlite3
+import time
 
+def moon_phase(percent_illuminated):
+    percent_illuminated *= 100
+    if percent_illuminated >= 99:
+        return 'Full Moon'
 def local_time(timezone, gmt_time):
     time_zones = {'America/Chicago': -6,
                   'US/Mountain': -7,
@@ -30,7 +35,7 @@ def local_time(timezone, gmt_time):
     time = str(gmt_time)
     local_hour = int(time[-8:-6]) + int(time_zones['{}'.format(timezone)])
     if local_hour < 0:
-        local_hour = local_hour + 24
+        local_hour += 24
     # local_time = time[:-8] + str(local_hour) + time[-6:]  #Date is included before time
     local_time = str(local_hour) + time[-6:]
     return str(local_time)
@@ -57,18 +62,26 @@ def main():
         print('Latitude: {}'.format(i[0]))
         print('Longitude: {}'.format(i[1]))
     
+    #Sunrise/Set
     ob = ephem.Observer()
     ob.lat = str(latitude)
     ob.lon = str(longitude)
+    ob.horizon = '-6'        #civil dusk/dawn is -6 degrees
     m = ephem.Sun()
     m.compute(ob)
     print(ob.next_rising(m))
     
-    twitter.statuses.update(status = 'The sun will rise at {} and set at {} today.'.format(
-                                                                                        local_time(time_zone, (ob.next_rising(m))), 
-                                                                                        local_time(time_zone, (ob.next_setting(m)))))
+   # twitter.statuses.update(status = 'In the city of {} the civil sunrise will be at {} and the civil sunset will be at {} today.'.format(
+   #                                                                                      city,
+   #                                                                                      local_time(time_zone, (ob.next_rising(m))), 
+   #                                                                                      local_time(time_zone, (ob.next_setting(m)))))
     print('Sun will rise: ' + local_time(time_zone, (ob.next_rising(m))))    
     print('Sun will set: '  + local_time(time_zone, (ob.next_setting(m))))
+    
+    #Phases of the moon
+    moon = ephem.Moon()
+    moon.compute(ob)
+    stage_of_moon = moon_phase(moon.moon_phase)
     
 if __name__ == '__main__':
     main()
