c153f97304eedfed29ff6fab0f85d93e3804360b
Arty52 <artgrichine@gmail.com>
2014-04-23 17:48:00 -0700

    Moon phase added
    Moon phase added
    twitter post is too long!

---
+++ README.txt
@@ -0,0 +1,20 @@
+Author:         Art Grichine
+Email:          ArtGrichine@gmail.com
+Assignment:     08
+Date:           23 April 2014
+
+Twitter account: @ArtGrichine
+
+What does this program do?
+This program will give the user civil dusk/dawn and moon phase from an input of city and state.
+
+Note:
+    1. 'astrotweet.py' code for tweeting on twitter of account @ArtGrichine
+    2. requirements.txt file is result of pip freeze on mysite in virtualenv. This
+        file contains 
+
+Use:
+To use this program type: python astrotweet.py "city of choice" States-initials
+                 Example: python astrotweet.py "Fullerton" ca
+                 
+tweets/output will be posted on www.twitter.com on account @ArtGrichine
--- astrotweet.py
+++ astrotweet.py
@@ -6,11 +6,30 @@ from twitter import *
 import sys
 import sqlite3
 import time
+from datetime import date, timedelta
 
-def moon_phase(percent_illuminated):
+def moon_phase(percent_illuminated, moon_lum_yesterday):
+    if percent_illuminated - moon_lum_yesterday >= 0:
+        phase = 'waxing'
+    else:
+        phase = 'waning '
+        
     percent_illuminated *= 100
     if percent_illuminated >= 99:
         return 'Full Moon'
+    elif percent_illuminated >= 51:
+        return phase + 'gibbous'
+    elif percent_illuminated >= 49:
+        if percent_illuminated - moon_lum_yesterday >= 0:
+            return 'First Quarter'
+        else:
+            return 'Last Quarter'
+    elif percent_illuminated >= 1:
+        return phase + 'crescent'
+    else:
+        return 'New Moon'
+        
+    # elif percent_illuminated >= 
 def local_time(timezone, gmt_time):
     time_zones = {'America/Chicago': -6,
                   'US/Mountain': -7,
@@ -49,7 +68,7 @@ def main():
                     'aEjd8gtlvPEu31it9MHF0rOPIrWKENiUzQeJ27qQNl2BJFyrHI'))    #API Secret
     
     city   = sys.argv[1] 
-    region = "US/" + sys.argv[2]                                       
+    region = "US/" + sys.argv[2].upper()                                    
     db = sqlite3.connect('us_only.sq3')
     c = db.cursor()
     
@@ -71,17 +90,33 @@ def main():
     m.compute(ob)
     print(ob.next_rising(m))
     
-   # twitter.statuses.update(status = 'In the city of {} the civil sunrise will be at {} and the civil sunset will be at {} today.'.format(
-   #                                                                                      city,
-   #                                                                                      local_time(time_zone, (ob.next_rising(m))), 
-   #                                                                                      local_time(time_zone, (ob.next_setting(m)))))
     print('Sun will rise: ' + local_time(time_zone, (ob.next_rising(m))))    
     print('Sun will set: '  + local_time(time_zone, (ob.next_setting(m))))
+    
+    #observer to compare for waxing/waning
+    ob2 = ephem.Observer()
+    ob2.lat = str(latitude)
+    ob2.lon = str(longitude)
+    yesterdays_date = str(date.today() - timedelta(1))      #yesterdays date
+    yesterdays_date = yesterdays_date[:4] + '/' + yesterdays_date[5:7] + '/' + yesterdays_date[8:]
+    ob2.date = yesterdays_date
+    ob2.horizon = '-6'      
+    moon_yesterday = ephem.Moon()
+    moon_yesterday.compute(ob2)
     
     #Phases of the moon
     moon = ephem.Moon()
     moon.compute(ob)
-    stage_of_moon = moon_phase(moon.moon_phase)
+    print(moon.moon_phase)
+    print(moon_yesterday.moon_phase)
+    stage_of_moon = moon_phase(moon.moon_phase, moon_yesterday.moon_phase)
+    print(stage_of_moon)
+    
+    twitter.statuses.update(status = 'In the city of {} the civil sunrise will be at {} and the civil sunset will be at {} today. The moon phase today is {}.'.format(
+                                                                                         city,
+                                                                                         local_time(time_zone, (ob.next_rising(m))), 
+                                                                                         local_time(time_zone, (ob.next_setting(m))),
+                                                                                         stage_of_moon))
     
 if __name__ == '__main__':
     main()
